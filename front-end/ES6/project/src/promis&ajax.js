e8e70b37a383c07e0689bd237d6dbe36700bd47b
lxn07122945 <lxn07122945@163.com>
2021-01-03 22:51:31 +0800

    promis

--- front-end/ES6/project/src/promis&ajax.js
+++ front-end/ES6/project/src/promis&ajax.js
@@ -24,6 +24,8 @@ function now () {
   */
 
 const staticSrc  = `http://api.map.baidu.com/panorama/v2?ak=FDQ7MYun6IydINVlh6G4Ic61E23OXv4a&width=512&height=256&location=116.313393,40.04778&fov=180`;
+const staticSrc2  = `http://api.map.baidu.com/panorama/v2?ak=FDQ7MYun6IydINVlh6G4Ic61E23OXv4a&width=513&height=256&location=116.313393,40.04778&fov=180`;
+const staticSrc3  = `http://api.map.baidu.com/panorama/v2?ak=FDQ7MYun6IydINVlh6G4Ic61E23OXv4a&width=514&height=256&location=116.313393,40.04778&fov=180`;
 
 const imgpromis = url => {
     return new Promise((resolve, reject) => {
@@ -37,14 +39,27 @@ const imgpromis = url => {
         }
     });
 };
+const p1 = imgpromis(staticSrc);
+    // p1.then(img => {
+    //     document.body.appendChild(img);
+    // })
+    // .catch(err => {
+    //     document.body.innerHTML = err;
+    // });
 
-imgpromis(staticSrc)
-    .then(img => {
-        document.body.appendChild(img);
-    })
-    .catch(err => {
-        document.body.innerHTML = err;
-    });
+const p2 = imgpromis(staticSrc2);
+const p3 = imgpromis(staticSrc3);
+
+// const pAll = Promise.all([p1, p2, p3]);
+// pAll.then(res => {
+//     console.log(res);
+// });
+
+const pRace = Promise.race([p1, p2, p3]);
+pRace.then(res => {
+    console.log(res);
+});
+
 
 /**
  * async 与await
@@ -55,4 +70,50 @@ async function f1 () {
     console.log(img);
     console.log('abc');
 }
-console.log(f1());
+
+async function f2 () {
+    // throw 3
+    // return Promise.reject(3);
+    // return 2
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(5)
+        }, 4000)
+    });
+};
+const value2 = f2();
+
+value2.then(
+    value => {
+        console.log('onResolved', value);
+    },
+    reason => {
+        console.log('onReject', reason);
+    }
+)
+
+
+// await
+function f3 () {
+    // throw 3
+    // return Promise.reject(3);
+    // return 2
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(6)
+        }, 4000)
+    });
+};
+
+async function fn3 () {
+    try {
+        const fn = await f3();
+    }
+    catch (err) {
+        const fn = err;
+        console.log(fn);
+
+    }
+}
+
+fn3();
